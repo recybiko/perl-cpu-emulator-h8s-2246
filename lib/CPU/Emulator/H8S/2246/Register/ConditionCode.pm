1624aa97fbc8df62ec6d6dae34adae66f8cf3cd8
CandyAngel <candyangel@electricjungle.org>
2019-06-17 18:43:10 +0100

    Create condition code register class

---
+++ lib/CPU/Emulator/H8S/2246/Register/ConditionCode.pm
@@ -0,0 +1,33 @@
+package CPU::Emulator::H8S::2246::Register::ConditionCode;
+use Mojo::Base '-base';
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+CPU::Emulator::H8S::2246::Register::ConditionCode - Condition Code Register
+for the H8S/2246
+
+=head1 SYNOPSIS
+
+  use CPU::Emulator::H8S::2246::Register::ConditionCode;
+
+  my $register = CPU::Emulator::H8S::2246::Register::ConditionCode->new;
+
+=head1 DESCRIPTION
+
+L<CPU::Emulator::H8S::2246::Register::ConditionCode> implements a Condition
+Code Register.
+
+=head1 METHODS
+
+L<CPU::Emulator::H8S::2246::Register::ConditionCode> inherits all methods from
+L<Mojo::Base>.
+
+=head1 SEE ALSO
+
+L<CPU::Emulator::H8S::2246>, L<Mojolicious>.
+
+=cut
