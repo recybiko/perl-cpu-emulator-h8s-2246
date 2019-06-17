ace6f7dbee6ee6ecb954ca3db34183b35e2a23e3
CandyAngel <candyangel@electricjungle.org>
2019-06-17 22:43:37 +0100

    Add from_file method

--- lib/CPU/Emulator/H8S/2246/Memory.pm
+++ lib/CPU/Emulator/H8S/2246/Memory.pm
@@ -2,8 +2,18 @@ package CPU::Emulator::H8S::2246::Memory;
 use Mojo::Base '-base';
 
 use Mojo::ByteStream;
+use Mojo::File;
 
 has bytes => sub { Mojo::ByteStream->new };
+
+sub from_file {
+  my ($self, $path) = @_;
+
+  my $bytes = $self->bytes;
+  $$bytes = Mojo::File->new($path)->slurp;
+
+  return $self;
+}
 
 1;
 
@@ -18,6 +28,7 @@ CPU::Emulator::H8S::2246::Memory - Memory for the H8S/2246 CPU
   use CPU::Emulator::H8S::2246::Memory;
 
   my $memory = CPU::Emulator::H8S::2246::Memory->new;
+  $memory->from_file($path);
 
 =head1 DESCRIPTION
 
@@ -36,7 +47,14 @@ Memory values, defaults to a L<Mojo::ByteStream> object.
 
 =head1 METHODS
 
-L<CPU::Emulator::H8S::2246::Memory> inherits all methods from L<Mojo::Base>.
+L<CPU::Emulator::H8S::2246::Memory> inherits all methods from L<Mojo::Base>
+and implements the following new ones.
+
+=head2 from_file
+
+  $memory = $memory->from_file($path);
+
+Loads the specified file into C<bytes>.
 
 =head1 SEE ALSO
 
--- t/cpu/emulator/h8s/2246/register/memory.t
+++ t/cpu/emulator/h8s/2246/register/memory.t
@@ -4,9 +4,16 @@ use Test::More;
 
 use CPU::Emulator::H8S::2246::Memory;
 
+use Mojo::File 'tempfile';
+
 my $memory = CPU::Emulator::H8S::2246::Memory->new;
 isa_ok $memory, 'CPU::Emulator::H8S::2246::Memory';
 
 isa_ok $memory->bytes, 'Mojo::ByteStream';
+
+my $file = tempfile;
+$file->spurt(pack('N', 0x1234_ABCD));
+$memory->from_file($file->path);
+is $memory->bytes, pack('N', 0x1234_ABCD), 'right value';
 
 done_testing;
