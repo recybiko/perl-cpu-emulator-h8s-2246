568e01beb1556667c384e95a162a004b765a2f8a
CandyAngel <candyangel@electricjungle.org>
2019-06-17 19:49:19 +0100

    Add registers to CPU class

--- lib/CPU/Emulator/H8S/2246.pm
+++ lib/CPU/Emulator/H8S/2246.pm
@@ -1,6 +1,14 @@
 package CPU::Emulator::H8S::2246;
 use Mojo::Base '-base';
 
+use CPU::Emulator::H8S::2246::Register::General;
+
+use Mojo::Collection 'c';
+
+has registers => sub {c(
+  map { CPU::Emulator::H8S::2246::Register::General->new } 0 .. 7
+)};
+
 1;
 
 =encoding utf8
@@ -19,6 +27,20 @@ CPU::Emulator::H8S::2246 - CPU emulator for the H8S/2246
 
 L<CPU::Emulator::H8S::2246> is an interpreting emulator for the H8S/2246 CPU,
 as found in the Cybiko Classic.
+
+=head1 ATTRIBUTES
+
+L<CPU::Emulator::H8S::2246> implements the following attributes.
+
+=head2 registers
+
+  my $collection = $cpu->registers;
+  $cpu = $cpu->registers(c(
+    map { CPU::Emulator::H8S::2246::Register::General->new } 0 .. 7
+  ));
+
+General registers, defaults to a L<Mojo::Collection> of 8
+L<CPU::Emulator::H8S::2246::Register::General> objects.
 
 =head1 METHODS
 
--- t/cpu/emulator/h8s/2246.t
+++ t/cpu/emulator/h8s/2246.t
@@ -7,4 +7,8 @@ use CPU::Emulator::H8S::2246;
 my $cpu = CPU::Emulator::H8S::2246->new;
 isa_ok $cpu, 'CPU::Emulator::H8S::2246';
 
+isa_ok $cpu->registers, 'Mojo::Collection';
+is $cpu->registers->size, 8, 'right value';
+isa_ok $cpu->registers->[0], 'CPU::Emulator::H8S::2246::Register::General';
+
 done_testing;
