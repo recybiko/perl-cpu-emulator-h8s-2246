9cec8fe0dc84b77dc35e2e437b34521ca73f9d3e
CandyAngel <candyangel@electricjungle.org>
2019-06-17 22:55:20 +0100

    Add read methods for 8, 16 and 32 bits

--- lib/CPU/Emulator/H8S/2246/Memory.pm
+++ lib/CPU/Emulator/H8S/2246/Memory.pm
@@ -15,6 +15,24 @@ sub from_file {
   return $self;
 }
 
+sub read16 {
+  my ($self, $offset) = @_;
+  my $bytes = $self->bytes;
+  return unpack('S>', substr $$bytes, $offset, 2);
+}
+
+sub read32 {
+  my ($self, $offset) = @_;
+  my $bytes = $self->bytes;
+  return unpack('N', substr $$bytes, $offset, 4);
+}
+
+sub read8 {
+  my ($self, $offset) = @_;
+  my $bytes = $self->bytes;
+  return unpack('C', substr $$bytes, $offset, 1);
+}
+
 1;
 
 =encoding utf8
@@ -55,6 +73,24 @@ and implements the following new ones.
   $memory = $memory->from_file($path);
 
 Loads the specified file into C<bytes>.
+
+=head2 read16
+
+  my $value = $memory->read16($offset);
+
+Returns the 2 byte (16 bit) value at the specified memory offset.
+
+=head2 read32
+
+  my $value = $memory->read32($offset);
+
+Returns the 4 byte (32 bit) value at the specified memory offset.
+
+=head2 read8
+
+  my $value = $memory->read8($offset);
+
+Returns the byte (8 bit) value at the specified memory offset.
 
 =head1 SEE ALSO
 
--- t/cpu/emulator/h8s/2246/register/memory.t
+++ t/cpu/emulator/h8s/2246/register/memory.t
@@ -16,4 +16,12 @@ $file->spurt(pack('N', 0x1234_ABCD));
 $memory->from_file($file->path);
 is $memory->bytes, pack('N', 0x1234_ABCD), 'right value';
 
+is $memory->read16(0), 0x1234, 'right value';
+is $memory->read16(2), 0xABCD, 'right value';
+is $memory->read32(0), 0x1234_ABCD, 'right value';
+is $memory->read8(0), 0x12, 'right value';
+is $memory->read8(1), 0x34, 'right value';
+is $memory->read8(2), 0xAB, 'right value';
+is $memory->read8(3), 0xCD, 'right value';
+
 done_testing;
