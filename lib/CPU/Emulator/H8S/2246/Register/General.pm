38f4ef78fd170469391fe8c231edb73120ca9011
CandyAngel <candyangel@electricjungle.org>
2019-06-17 19:10:54 +0100

    Add 32bit read/write methods

--- lib/CPU/Emulator/H8S/2246/Register/General.pm
+++ lib/CPU/Emulator/H8S/2246/Register/General.pm
@@ -1,6 +1,20 @@
 package CPU::Emulator::H8S::2246::Register::General;
 use Mojo::Base '-base';
 
+has _value => 0;
+
+sub read32 {
+  my $self = shift;
+  my $value = $self->_value & 0xFFFF_FFFF;
+  return $value;
+}
+
+sub write32 {
+  my ($self, $value) = @_;
+  $value = $value & 0xFFFF_FFFF;
+  return $self->_value($value);;
+}
+
 1;
 
 =encoding utf8
@@ -15,6 +29,8 @@ H8S/2246
   use CPU::Emulator::H8S::2246::Register::General;
 
   my $register = CPU::Emulator::H8S::2246::Register::General->new;
+  $register->write32(1024);
+  my $value = $register->read32;
 
 =head1 DESCRIPTION
 
@@ -23,7 +39,19 @@ L<CPU::Emulator::H8S::2246::Register::General> implements a General Register.
 =head1 METHODS
 
 L<CPU::Emulator::H8S::2246::Register::General> inherits all methods from
-L<Mojo::Base>.
+L<Mojo::Base> and implements the following new ones.
+
+=head2 read32
+
+  my $value = $register->read32;
+
+Returns the lowest 4 bytes (32 bits) of the register.
+
+=head2 write32
+
+  my $register = $register->write32(1024);
+
+Writes the lowest 4 bytes (32 bits) of the provided value to the register.
 
 =head1 SEE ALSO
 
--- t/cpu/emulator/h8s/2246/register/general.t
+++ t/cpu/emulator/h8s/2246/register/general.t
@@ -7,4 +7,8 @@ use CPU::Emulator::H8S::2246::Register::General;
 my $register = CPU::Emulator::H8S::2246::Register::General->new;
 isa_ok $register, 'CPU::Emulator::H8S::2246::Register::General';
 
+is $register->read32, '0', 'right value';
+is $register->write32(0xFFFF_FFFF)->read32, 0xFFFF_FFFF, 'right value';
+is $register->write32(0)->read32, 0, 'right value';
+
 done_testing;
